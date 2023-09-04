7a604d6861ac90423ebd8e5874d5d155707fa011
Aleksandr Ataev <onlyaa94@mail.ru>
2023-09-04 21:45:03 +0300

    Initial Commit

---
+++ TestApp-4Screens/TestApp_4ScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp_4ScreensApp.swift
+//  TestApp-4Screens
+//
+//  Created by Aleksandr Ataev on 04.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp_4ScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
